3465044eff16d94a65d5a5dfcc7deb99baf5d5a7
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-03 03:51:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ attr_accessor :side0, :side1, :side2, :equilateral, :isosceles, :scalene, :trian
             puts error.message
         end
       end
-    if (@side0 + @side1) <  @side2 || @side0 > (@side1 + @side2) || (@side0 + @side2) < @side1
+    if (@side0 + @side1) <  @side2 || (@side1 + @side2) < @side0 || (@side0 + @side2) < @side1
       begin
         raise TriangleInequality.new
       rescue TriangleInequality => error
