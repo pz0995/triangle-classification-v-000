ef6ad00a8f13006f5f44492349cfdb0411e86562
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-03 00:22:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,57 @@
 class Triangle
-  # write code here
+attr_accessor :side0, :side1, :side2, :equilateral, :isosceles, :scalene
+
+  def initialize(side0:, side1:, side2:)
+    triangle_array = [@side0, @side1, @side2]
+    @side0 = side0
+    @side1 = side1
+    @side2 = side2
+  end
+
+  def kind
+    # (equilateral:, isosceles:, scalene:)
+    # case triangle_array
+        triangle_array = [@side0, @side1, @side2]
+    if @side0 == @side1 && @side1 == @side2
+      return :equilateral
+    elsif @side1 == @side2 || @side0 == @side2 || @side0 == @side1 && (triangle_array.uniq.length == triangle_array.length).count == 1
+      return :isosceles
+    (triangle_array.uniq.length > 1) ? false : true
+    if true then return :scalene
+end
+
+    else triangle_array.uniq.length == triangle_array.length
+      return :scalene
+
+    when [0] < 0 || [1] < 0 || [2] < 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    when (@side0 + @side1) <  @side2 || @side0 > (@side1) + @side2 || (@side0 + @side2) < @side1
+      return
+      begin
+        raise TriangleInequality
+      rescue TriangleInequality => error
+          puts error.message
+      end
+
+    end
+  end
+end
+
+
+
+    class TriangleInequality < StandardError
+      def message
+        "Error: the sum of two sides must be greater than the third side"
+      end
+    end
+
+    class TriangleError < StandardError
+      def message
+        "Error: each triangle length must be greater than 0"
+    #   end
+    end
 end
