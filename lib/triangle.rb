63800a94612d22fc2f780ae08be79c47bfee9948
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-03 02:12:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,15 +3,14 @@ attr_accessor :side0, :side1, :side2, :equilateral, :isosceles, :scalene
 
   def initialize(side0:, side1:, side2:)
     triangle_array = [@side0, @side1, @side2]
-    @side0 = triangle_array[0]
-    @side1 = triangle_array[1]
-    @side2 = triangle_array[2]
+      @side0 = triangle_array[0]
+      @side1 = triangle_array[1]
+      @side2 = triangle_array[2]
   end
 
   def kind
     # (equilateral:, isosceles:, scalene:)
     # case triangle_array
-    triangle_array = [@side0, @side1, @side2]
     if @side0 == @side1 && @side1 == @side2
       return :equilateral
     elsif @side1 == @side2 || @side0 == @side2 || @side0 == @side1 && (triangle_array.uniq.length == triangle_array.length).count == 1
